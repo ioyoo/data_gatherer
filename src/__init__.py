e838349c35fbf081379234be629909f2d3f76bb1
Andrés Naves <andres.naves.mauri@hp.com>
2022-11-11 11:06:52 +0100

    Small bug fix

--- src/__init__.py
+++ src/__init__.py
@@ -5,10 +5,10 @@ import time
 
 def main():
     # reddit = RedditCrawler(CLIENT_AUTH2, SECRET, USER_AGENT)
-    # start_time = time.time()
+    start_time = time.time()
     # reddit.get_info_from(
     #     stock_names=["TSLA", "tesla", "short", "HBO"], limit=20, )
-    # print("--- %s seconds ---" % (time.time() - start_time))
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 if __name__ == "__main__":
