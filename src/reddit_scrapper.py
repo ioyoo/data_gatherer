ad232d5fa1a521e51b2bad71857cb04c645293bc
Andrés Naves <andres.naves.mauri@hp.com>
2022-12-28 15:47:31 +0100

    Now reddit_scrapper contains functions to print text found or to return it as a dictionary

--- src/reddit_scrapper.py
+++ src/reddit_scrapper.py
@@ -1,4 +1,4 @@
-from .tools.text_tools import find_in_text
+from tools.text_tools import find_in_text
 import praw
 
 
@@ -12,12 +12,44 @@ class RedditCrawler:
             client_secret=client_secret,
             user_agent=user_agent,
         )
+        self.dict = {
+
+        }
 
     def get_info_from(self,
                       stock_names=["GME", "gamestop"],
                       subreddits=["wallstreetbets"],
                       limit=5):
-        """ Given stock anmes and subreddits, returns how many times it was mentioned, given a time lot
+        """ Given stock names and subreddits, returns how many times it was mentioned, given a time lot
+
+        Args:
+            stock_names (list, optional): _description_. Defaults to ["GME"].
+            subreddits (list, optional): _description_.
+                        Defaults to ["wallstreetbets"].
+            timeout (int, optional): _description_. Defaults to 5.
+
+        Returns: dict with data. (text, upvotes, downvotes, awards, category)
+        """
+        for subreddit in subreddits:
+            sub = self.reddit.subreddit(subreddit)
+            for submission in sub.new(limit=limit):
+                text = submission.selftext
+                for word in stock_names:
+                    if find_in_text(word, text):
+                        self.dict[submission.id] = {
+                            "text": submission.selftext,
+                            "upvotes": submission.ups,
+                            "downvotes": submission.downs,
+                            "awards": submission.total_awards_received,
+                            "category": submission.category
+                        }
+        return self.dict
+
+    def read_from_reddit(self,
+                         stock_names=["GME", "gamestop"],
+                         subreddits=["wallstreetbets"],
+                         limit=5):
+        """ Given stock names and subreddits, print in terminal the found messages
 
         Args:
             stock_names (list, optional): _description_. Defaults to ["GME"].
@@ -32,3 +64,6 @@ class RedditCrawler:
                 for word in stock_names:
                     if find_in_text(word, text):
                         print(text + "\n")
+
+    def get_data(self):
+        return self.dict
