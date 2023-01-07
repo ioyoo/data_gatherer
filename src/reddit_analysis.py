9015e3fd9401ad7afc818811df4571eddf29efe1
Andrés Naves <andres.naves.mauri@hp.com>
2023-01-07 19:07:46 +0100

    Reworked strcture. Added method to get top 100 stock names. Creted post service to communicate with API

--- src/reddit_analysis.py
+++ src/reddit_analysis.py
@@ -4,28 +4,31 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 class RedditAnalysis:
     def __init__(self) -> None:
         self.results = {}
-        
+
     def run_analysis(self, posts: dict):
         analyzer = SentimentIntensityAnalyzer()
 
         for key, post_list in posts.items():
             for post in post_list:
-                self.sentiment_analyzer_scores(key, post['text'], post['upvotes'], post['downvotes'], analyzer)
+                self.sentiment_analyzer_scores(
+                    key, post['text'], post['upvotes'], post['downvotes'], analyzer)
             self.results[key] /= len(post_list)
-            
+
         return self.results
 
     def sentiment_analyzer_scores(self, key, text, upvotes, downvotes, analyzer: SentimentIntensityAnalyzer):
         score = analyzer.polarity_scores(text)
         try:
-            self.results[key] = self.results[key] + self.get_comment_value(score, upvotes, downvotes)
+            self.results[key] = self.results[key] + \
+                self.get_comment_value(score, upvotes, downvotes)
         except KeyError:
-            self.results[key] = self.get_comment_value(score, upvotes, downvotes)
-    
+            self.results[key] = self.get_comment_value(
+                score, upvotes, downvotes)
+
     def get_comment_value(self, score, upvotes, downvotes):
         score.popitem()
         max_value = max(score.values())
-        max_key = max(score, key=score.get())
+        max_key = max(score, key=score.get)
         match max_key:
             case 'neg':
                 return -max_value + (0.1 * upvotes) - (0.1 * downvotes)
